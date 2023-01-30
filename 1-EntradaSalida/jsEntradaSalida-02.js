b9b96e7ccedcd4c769b9575efe0dab4f2abc6108
MarianaMrtz <marianasidluski@gmail.com>
2023-01-30 15:40:19 -0300

    Guia 1, ej 1 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -2,7 +2,18 @@
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
+
 {
-	alert("ok");
+	// alert("ok");
+
+	// var edad;
+	let nombre;
+
+	nombre = prompt("ingrese un nombre")
+	
+    nombre = "mario"; // asignacion de string. 
+
+    // mostrar por alert
+    alert(nombre);
 }
 
